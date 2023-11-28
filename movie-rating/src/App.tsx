706e422c45f7d859c457065f22e6c9e08731c426
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-11-28 14:05:17 +0200

    install semantic-ui-css dependency and creating routing

--- movie-rating/src/App.tsx
+++ movie-rating/src/App.tsx
@@ -1,10 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Navbar } from "./components/navbar";
 import "./App.css";
 
 function App() {
     return (
         <div>
             <Router>
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<h1>Home</h1>} />
                     <Route path="/auth" element={<h1>Auth</h1>} />
