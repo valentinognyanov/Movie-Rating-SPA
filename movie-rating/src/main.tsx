f4f3e2630ba485e3ef9666ea7ecb014077fd0cd7
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-01 14:06:27 +0200

    config query client

--- movie-rating/src/main.tsx
+++ movie-rating/src/main.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <App />
+        <QueryClientProvider client={queryClient}>
+            <App />
+        </QueryClientProvider>
     </React.StrictMode>
 );
