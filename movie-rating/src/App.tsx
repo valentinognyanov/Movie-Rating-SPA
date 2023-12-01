f0917c312bef291ea89ce2e4bb13c2c5367408e5
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-01 14:01:19 +0200

    update App.tsx

--- movie-rating/src/App.tsx
+++ movie-rating/src/App.tsx
@@ -1,5 +1,8 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { Navbar } from "./components/navbar";
+
+import {Navbar} from "./components/navbar";
+import {Auth} from "./pages/auth";
+
 import "./App.css";
 
 function App() {
@@ -9,7 +12,7 @@ function App() {
                 <Navbar />
                 <Routes>
                     <Route path="/" element={<h1>Home</h1>} />
-                    <Route path="/auth" element={<h1>Auth</h1>} />
+                    <Route path="/auth" element={<Auth />} />
                     <Route path="/rated" element={<h1>Rated</h1>} />
                 </Routes>
             </Router>
