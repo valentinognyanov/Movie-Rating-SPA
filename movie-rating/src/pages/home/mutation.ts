b73975dbefd456f9e23c9a80eec190f21e16bbf2
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-02 15:31:07 +0200

    update the rating service

--- movie-rating/src/pages/home/mutation.ts
+++ movie-rating/src/pages/home/mutation.ts
@@ -1,34 +1,38 @@
 export const rateMovie = async (movieId: number, rating: number) => {
+    const options = {
+        method: "POST",
+        headers: {
+            accept: "application/json",
+            "Content-Type": "application/json;charset=utf-8",
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNGY1NDViM2YwOTgxNmFmN2Q1MmFlZWUxNjAxMjFjNSIsInN1YiI6IjY1NjVkODliNmMwYjM2MDEwMWZiM2MwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H9Ptj7U_4a_YAQIphMJPxZiwR2U8msKTEaPFly-3aLE",
+        },
+        body: `{"value":${rating}}`,
+    };
+
     const res = await fetch(
-        `https://api.themoviedb.org/3/movie/${movieId}/guest_session_id=${localStorage.getItem(
-            "guest_session_id"
-        )}&api_key=${import.meta.env.VITE_API_KEY}`,
-        {
-            method: "POST",
-            headers: {
-                accept: "application/json",
-                "content-type": "application/json;charset=utf-8",
-            },
-            body: `{"value": ${rating}}`,
-        }
+        `https://api.themoviedb.org/3/movie/${movieId}/rating`,
+        options
     );
 
     return res.json();
 };
 
 export const rateTvShow = async (tvShowId: number, rating: number) => {
+    const options = {
+        method: "POST",
+        headers: {
+            accept: "application/json",
+            "Content-Type": "application/json;charset=utf-8",
+            Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNGY1NDViM2YwOTgxNmFmN2Q1MmFlZWUxNjAxMjFjNSIsInN1YiI6IjY1NjVkODliNmMwYjM2MDEwMWZiM2MwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H9Ptj7U_4a_YAQIphMJPxZiwR2U8msKTEaPFly-3aLE",
+        },
+        body: `{"value":${rating}}`,
+    };
+
     const res = await fetch(
-        `https://api.themoviedb.org/3/tv/${tvShowId}/guest_session_id=${localStorage.getItem(
-            "guest_session_id"
-        )}&api_key=${import.meta.env.VITE_API_KEY}`,
-        {
-            method: "POST",
-            headers: {
-                accept: "application/json",
-                "content-type": "application/json;charset=utf-8",
-            },
-            body: `{"value": ${rating}}`,
-        }
+        `https://api.themoviedb.org/3/tv/${tvShowId}/rating`,
+        options
     );
 
     return res.json();
