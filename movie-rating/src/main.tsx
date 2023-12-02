e1377d5e2c5a33305edccc113849101fd578d825
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-02 15:33:56 +0200

    add ToastContainer in main.tsx

--- movie-rating/src/main.tsx
+++ movie-rating/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {ToastContainer} from "react-toastify";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
             <App />
+            <ToastContainer />
         </QueryClientProvider>
     </React.StrictMode>
 );
