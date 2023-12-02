64599b48e323ccd8f4ab98fad1923287cccb8a86
Valentin Ognyanov <106106614+valentinognyanov@users.noreply.github.com>
2023-12-02 18:38:48 +0200

    create logout functionality

--- movie-rating/src/components/navbar.tsx
+++ movie-rating/src/components/navbar.tsx
@@ -1,7 +1,15 @@
-import {Menu} from "semantic-ui-react";
-import {Link} from "react-router-dom";
+import {Menu, Button} from "semantic-ui-react";
+import {Link, useNavigate} from "react-router-dom";
 
 export const Navbar = () => {
+    const isLoggedIn = localStorage.getItem("guest_session_id") !== null;
+    const navigate = useNavigate();
+
+    const logout = () => {
+        localStorage.removeItem("guest_session_id");
+        navigate("/auth");
+    };
+
     return (
         <Menu fixed="top" size="huge">
             <Menu.Item as={Link} to="/" style={{fontSize: "1.5rem"}}>
@@ -13,9 +21,23 @@ export const Navbar = () => {
             </Menu.Item>
 
             <Menu.Menu position="right">
-                <Menu.Item as={Link} to="/auth" style={{fontSize: "1.5rem"}}>
-                    Auth
-                </Menu.Item>
+                {isLoggedIn ? (
+                    <Menu.Item
+                        as={Button}
+                        style={{fontSize: "1.5rem"}}
+                        onClick={logout}
+                    >
+                        Logout
+                    </Menu.Item>
+                ) : (
+                    <Menu.Item
+                        as={Link}
+                        to="/auth"
+                        style={{fontSize: "1.5rem"}}
+                    >
+                        Auth
+                    </Menu.Item>
+                )}
             </Menu.Menu>
         </Menu>
     );
